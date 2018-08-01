38fe4f69bf312d8374fd2e6e35a186bc1636cf19
Naveed <nsaleem@edx.org>
2018-08-01 15:20:18 +0500

    Update test_android_contacts.py

--- test_android_contacts.py
+++ test_android_contacts.py
@@ -8,7 +8,7 @@ PATH = lambda p: os.path.abspath(
     os.path.join(os.path.dirname(__file__), p)
 )
 APPIUM_LOCAL_HOST_URL = 'http://localhost:4723/wd/hub'
-PLATFORM_VERSION = '6.0'
+PLATFORM_VERSION = '7.0'
 
 
 class TestWebViewAndroid():
